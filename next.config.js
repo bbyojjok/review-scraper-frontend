9039ef235990d079c235497f893facd12bb1452a
bbyojjok <bbyojjok@gmail.com>
2023-09-27 10:20:46 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'is1-ssl.mzstatic.com',
+      'is2-ssl.mzstatic.com',
+      'is3-ssl.mzstatic.com',
+      'is4-ssl.mzstatic.com',
+      'is5-ssl.mzstatic.com',
+      'is6-ssl.mzstatic.com',
+      'is7-ssl.mzstatic.com',
+      'is8-ssl.mzstatic.com',
+      'is9-ssl.mzstatic.com',
+      'play-lh.googleusercontent.com',
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8082/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
